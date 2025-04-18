9c5b10d1f802797488acff8c945c3cdc4958edee
Daniel Krastev <danielkrastev01@gmail.com>
2025-04-18 18:49:52 +0300

    Created requester

---
+++ src/utils/requester.js
@@ -0,0 +1,47 @@
+const request = async (method, url, data, options = {}) => {
+    if(method != 'GET'){
+        options.method = method;
+    }
+
+    if(data) {
+        if(data instanceof FormData){
+            options.body = data;
+        }else{
+            options = {
+                ...options,
+                headers: {
+                    'Content-Type': 'application/json',
+                    ...options.headers,
+                },
+                body: JSON.stringify(data),
+            };
+        };
+    };
+
+    try{
+        const response = await fetch(url, options);
+        const responseContentType = response.headers.get('Content-Type');
+        if(!responseContentType){
+            return;
+        }
+
+        const result = await response.json();
+
+        if(!response.ok){
+            throw new Error(result.error);
+        }
+
+        return result;
+    }catch(err){
+        throw err;
+    }
+
+}
+
+export default {
+    get: request.bind(null, 'GET'),
+    post: request.bind(null, 'POST'),
+    put: request.bind(null, 'PUT'),
+    patch: request.bind(null, 'PATCH'),
+    delete: request.bind(null, 'DELETE'),
+}
