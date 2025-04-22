b2b138abdad8090a22a0847598ce142bcb5bf1a2
Daniel Krastev <danielkrastev01@gmail.com>
2025-04-22 23:32:16 +0300

    Update delete drawer modal

--- src/components/admin/partials/delete-drawer/DeleteDrawer.jsx
+++ src/components/admin/partials/delete-drawer/DeleteDrawer.jsx
@@ -11,7 +11,7 @@ export default function DeleteDrawer({
                 tabIndex="-1"
             >
                 <h5 className="inline-flex items-center text-sm font-semibold text-gray-500 uppercase" id="drawer-delete-label">
-                    Delete item
+                    Изтриване на запис
                 </h5>
                 <button
                     aria-controls="drawer-delete-product-default"
@@ -26,7 +26,7 @@ export default function DeleteDrawer({
                             d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                         />
                     </svg>
-                    <span className="sr-only">Close menu</span>
+                    <span className="sr-only">Затвори</span>
                 </button>
                 <svg className="w-10 h-10 mt-8 mb-4 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path
@@ -36,20 +36,20 @@ export default function DeleteDrawer({
                         d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                     />
                 </svg>
-                <h3 className="mb-6 text-lg text-gray-500">Are you sure you want to delete this product?</h3>
+                <h3 className="mb-6 text-lg text-gray-500">Сигурни ли сте, че искате да изтриете този запис?</h3>
                 <div className="flex space-x-4">
                     <Link
                         to="#"
                         className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center"
                     >
-                        Yes, I'm sure
+                        Да, сигурен съм
                     </Link>
                     <Link
                         to="#"
                         onClick={closeDrawerDelete}
                         className="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 border border-gray-200 font-medium rounded-lg text-sm px-3 py-2.5 text-center"
                     >
-                        No, cancel
+                        Отказ
                     </Link>
                 </div>
             </div>
