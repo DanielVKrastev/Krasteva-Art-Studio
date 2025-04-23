d2152b7cba06f5c6a886c80bd48ca209525e0e4e
Daniel Krastev <danielkrastev01@gmail.com>
2025-04-24 00:07:37 +0300

    Use admin provder and guard in admin layout

--- src/layouts/admin-layout/AdminLayout.jsx
+++ src/layouts/admin-layout/AdminLayout.jsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/admin/sidebar/Sidebar";
+import AdminProvider from "../../providers/AdminProvider";
+import PrivateAdminGuard from "../../guards/PrivateAdminGuard";
 
 export default function AdminLayout() {
   return (
-    <div className="admin-wrapper min-h-screen flex flex-col">
-      
-      <Sidebar />
+    <AdminProvider>
+      <PrivateAdminGuard>
+        <div className="admin-wrapper min-h-screen flex flex-col">
 
-      <main className="flex-1 p-6">
-        <Outlet />
-      </main>
-    </div>
+          <Sidebar />
+
+          <main className="flex-1 p-6">
+            <Outlet />
+          </main>
+        </div>
+        </PrivateAdminGuard>
+        </AdminProvider>
   );
 }
