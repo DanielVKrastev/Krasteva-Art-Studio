caa4ad7639153b555724b88e1334385a96049963
Daniel Krastev <danielkrastev01@gmail.com>
2025-04-24 00:07:23 +0300

    Create Private admin guard

---
+++ src/guards/PrivateAdminGuard.jsx
@@ -0,0 +1,19 @@
+import { Navigate, useLocation } from "react-router-dom";
+import { useContext } from "react";
+import { AdminContext } from "../contexts/AdminContext";
+
+export default function PrivateAdminGuard({ children }) {
+  const { admin, loading } = useContext(AdminContext);
+  const location = useLocation();
+  console.log(admin);
+  
+  if (loading) {
+    return <div>Зареждане...</div>; // or loader/spinner
+  }
+
+  if (!admin) {
+    return <Navigate to="/admin/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
