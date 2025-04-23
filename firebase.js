104b28b12becb3e099f80c2fc5730861702c0348
Daniel Krastev <danielkrastev01@gmail.com>
2025-04-24 00:06:48 +0300

    Created Admin Context

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { browserLocalPersistence, getAuth, setPersistence } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,5 +14,20 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+const auth = getAuth(app);
 
-export { app, database };
+setPersistence(auth, browserLocalPersistence)
+  .then(() => {
+    // Handle post-persistence setup without redirecting or refreshing
+    console.log("Persistence has been set successfully.");
+    // Optionally, update app state or call a function
+
+    // Refresh current page when persistence is loaded, little hack
+    //page.redirect(location.pathname)
+  })
+  .catch(err => {
+    console.error("Failed to set persistence:", err);
+  })
+
+
+export { app, auth, database };
