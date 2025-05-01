8862cb889764e8f0caffd3360223185b600f6bed
Daniel Krastev <danielkrastev01@gmail.com>
2025-05-02 00:09:11 +0300

    Added Loading spinner in art categories and new arts

--- src/components/partials/art-categories/ArtCategories.jsx
+++ src/components/partials/art-categories/ArtCategories.jsx
@@ -1,57 +1,65 @@
 import { useEffect, useState } from "react";
 import categoryApi from "../../../api/categoryApi";
 import { Link } from "react-router-dom";
+import LoadingSpinner from "../loading-spinner/LoadingSpinner";
 
 export default function ArtCategories() {
     const [categories, setCategories] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-        useEffect(() => {
-            const fetchInitial = async () => {
-                const data = await categoryApi.getAll();
-                setCategories(data);
-            };
-            fetchInitial();
-        }, []);
+    useEffect(() => {
+        const fetchInitial = async () => {
+            setIsLoading(true);
+            const data = await categoryApi.getAll();
+            setIsLoading(false);
+            setCategories(data);
+        };
+        fetchInitial();
+    }, []);
 
     return (
-        <section className="py-12 border border-gray-300">
-           <hr className="w-10 mx-auto border-t-2 border-indigo-700 text-center" />
-            <div className="max-w-7xl mx-auto px-4">
-              
-                <div className="text-center mb-10">
-                    <h2 className="text-3xl font-bold">Категории</h2>
+        <>
+            {isLoading && <LoadingSpinner />}
+
+            <section className="py-12 border border-gray-300">
+                <hr className="w-10 mx-auto border-t-2 border-indigo-700 text-center" />
+                <div className="max-w-7xl mx-auto px-4">
+
+                    <div className="text-center mb-10">
+                        <h2 className="text-3xl font-bold">Категории</h2>
+                    </div>
+
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+
+
+                        {categories.map((category, index) => (
+                            <Link
+                                key={index}
+                                to={`/artshop?category=${category?.name}`}
+                                className="relative group overflow-hidden rounded-2xl shadow-lg"
+                                data-aos="fade"
+                                data-aos-delay={index * 100}
+                            >
+                                <div className="overflow-hidden m-0 p-0">
+                                    <img
+                                        src={category?.imageUrl}
+                                        alt={category?.name}
+                                        className="w-full h-72 object-cover transform transition-transform duration-500 group-hover:scale-110"
+                                    />
+                                </div>
+
+                                <div className="absolute inset-0 bg-black bg-opacity-50 group-hover:bg-opacity-60 transition duration-300" style={{ opacity: 0.5 }} />
+
+                                <div className="absolute inset-0 flex flex-col items-center justify-center text-white px-4 text-center">
+                                    <span className="text-sm uppercase tracking-wide text-gray-200">Категория</span>
+                                    <h3 className="text-2xl font-bold mt-2">{category?.name}</h3>
+                                </div>
+                            </Link>
+
+                        ))}
+                    </div>
                 </div>
-
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                    
-                 
-                    {categories.map((category, index) => (
-                        <Link
-                            key={index}
-                            to={`/artshop?category=${category?.name}`}
-                            className="relative group overflow-hidden rounded-2xl shadow-lg"
-                            data-aos="fade"
-                            data-aos-delay={index * 100}
-                        >
-                            <div className="overflow-hidden m-0 p-0">
-                                <img
-                                    src={category?.imageUrl}
-                                    alt={category?.name}
-                                    className="w-full h-72 object-cover transform transition-transform duration-500 group-hover:scale-110"
-                                />
-                            </div>
-
-                            <div className="absolute inset-0 bg-black bg-opacity-50 group-hover:bg-opacity-60 transition duration-300" style={{ opacity: 0.5 }}/>
-
-                            <div className="absolute inset-0 flex flex-col items-center justify-center text-white px-4 text-center">
-                                <span className="text-sm uppercase tracking-wide text-gray-200">Категория</span>
-                                <h3 className="text-2xl font-bold mt-2">{category?.name}</h3>
-                            </div>
-                        </Link>
-                      
-                    ))}
-                </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 }
--- src/components/partials/new-arts/NewArts.jsx
+++ src/components/partials/new-arts/NewArts.jsx
@@ -1,37 +1,42 @@
 import { useEffect, useState } from "react";
 import paintingApi from "../../../api/paintingApi";
-import { Link } from "react-router-dom";
-import PaintingCart from "../paiting-card/PaintingCard";
 import PaintingCard from "../paiting-card/PaintingCard";
+import LoadingSpinner from "../loading-spinner/LoadingSpinner";
 
 export default function NewArts() {
     const [paintings, setPaintings] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         (async function () {
+            setIsLoading(true);
             const paintings = await paintingApi.getLimit(3);
+            setIsLoading(false);
             setPaintings(paintings);
         })();
     }, []);
 
     return (
-        <section className="py-12 border border-gray-300">
-            <div className="max-w-7xl mx-auto px-4">
-                <hr className="w-10 mx-auto border-t-2 border-indigo-700 text-center" />
-                <div className="text-center mb-10">
-                    <h2 className="text-3xl font-bold">Нови картини</h2>
-                </div>
-                {paintings.length === 0 ? (
-                    <p className="text-gray-600">Няма налични картини</p>
-                ) : (
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                        {paintings.map((paint) => (
-                            <PaintingCard key={paint.id} paint={paint} />
-                        ))}
-                    </div>
-                )}
-            </div>
-        </section>
+        <>
+            {isLoading && <LoadingSpinner />}
 
+            <section className="py-12 border border-gray-300">
+                <div className="max-w-7xl mx-auto px-4">
+                    <hr className="w-10 mx-auto border-t-2 border-indigo-700 text-center" />
+                    <div className="text-center mb-10">
+                        <h2 className="text-3xl font-bold">Нови картини</h2>
+                    </div>
+                    {paintings.length === 0 ? (
+                        <p className="text-gray-600">Няма налични картини</p>
+                    ) : (
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                            {paintings.map((paint) => (
+                                <PaintingCard key={paint.id} paint={paint} />
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </section>
+        </>
     );
 }
