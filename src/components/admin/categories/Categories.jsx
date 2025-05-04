eaf5880449da5823b4b2796782c38feb93711178
Daniel Krastev <danielkrastev01@gmail.com>
2025-05-04 19:56:11 +0300

    Search category

--- src/components/admin/categories/Categories.jsx
+++ src/components/admin/categories/Categories.jsx
@@ -14,7 +14,8 @@ export default function Paintings() {
     const [categories, setCategories] = useState([]);
     const [recordsPerPage, setRecordsPerPage] = useState(10);
     const [isLoading, setIsLoading] = useState(false);
-    
+    const [searchParams, setSearchParams] = useState([]);
+
     const [currentPage, setCurrentPage] = useState(1);
 
     const totalPages = Math.ceil(categories.length / recordsPerPage);
@@ -45,41 +46,65 @@ export default function Paintings() {
     useEffect(() => {
         const fetchInitial = async () => {
             setIsLoading(true);
-            const data = await categoryApi.getAll();
-            setIsLoading(false);
-            setCategories(data);
+            if (!searchParams.search) {
+                await categoryApi.getAll()
+                    .then(result => {
+                        setCategories(result);
+                        setIsLoading(false);
+                    }).catch(err => {
+                        setIsLoading(false);
+                        console.error(err.message);
+                    });
+                return;
+            }
+
+            await categoryApi.getAll()
+                .then(result => {
+                    const search = searchParams.search;
+                    const criteria = 'name';
+
+                    const searchFind = result.filter(category =>
+                        category[criteria]?.toLowerCase().includes(search.toLowerCase())
+                    );
+                    setCategories(searchFind);
+                    setIsLoading(false);
+                }).catch(err => {
+                    setIsLoading(false);
+                    console.error(err);
+                });
+            return;
         };
         fetchInitial();
-    }, [recordsPerPage, isOpenDelete, isOpenCreate, isOpenUpdate]);
+    }, [recordsPerPage, isOpenDelete, isOpenCreate, isOpenUpdate, searchParams]);
 
     const deletePainting = async (id) => {
-        try{
+        try {
             const painting = await categoryApi.getOne(id);
             await categoryApi.deleteCategory(id);
             setCategories(state => state.filter(painting => painting.id !== id));
-            if(painting.deletehash){
+            if (painting.deletehash) {
                 deleteImage(painting.deletehash);
             }
-        }catch(err){
+        } catch (err) {
             console.log(err);
         }
     };
 
-   
+
 
     const openDrawerCreate = () => {
         setIsOpenCreate(true);
     };
- 
+
     const closeDrawerCreate = () => {
         setIsOpenCreate(false);
     };
 
     const openDrawerUpdate = (id, name) => {
         setIsOpenUpdate(true);
-        setUpdateItem({id, name});
+        setUpdateItem({ id, name });
     };
- 
+
     const closeDrawerUpdate = () => {
         setIsOpenUpdate(false);
         setUpdateItem(null);
@@ -87,7 +112,7 @@ export default function Paintings() {
 
     const openDrawerDelete = (id, name) => {
         setIsOpenDelete(true);
-        setDeleteItem({id, name});
+        setDeleteItem({ id, name });
     };
 
     const closeDrawerDelete = () => {
@@ -95,10 +120,19 @@ export default function Paintings() {
         setDeleteItem(null);
     };
 
+    const searchClickHandler = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const search = formData.get('search');
+        const criteria = formData.get('criteria');
+
+        setSearchParams({ search, criteria });
+    };
+
     return (
         <>
             {isLoading && <LoadingSpinner />}
-            
+
             <div className="p-6 bg-white text-gray-900 sm:ml-55 block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5">
                 <div className="mt-14">
                     <div className="mb-4">
@@ -112,32 +146,56 @@ export default function Paintings() {
 
                     <div className="items-center justify-between block sm:flex">
                         <div className="flex items-center mb-4 sm:mb-0">
-                            <form className="sm:pr-3" action="#" method="GET">
-                                <label htmlFor="products-search" className="sr-only">Търси</label>
-                                <div className="relative w-48 mt-1 sm:w-64 xl:w-96">
-                                    <input
-                                        type="text"
-                                        id="products-search"
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
-                                        placeholder="Търсене на категории"
-                                    />
+                            <form className="w-100 mx-auto" onSubmit={searchClickHandler}>
+                                <div className="flex">
+                                    <div className="relative w-full">
+                                        <input
+                                            type="search"
+                                            name="search"
+                                            className="block p-3 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                                            placeholder="Категория..."
+                                        />
+                                        <button
+                                            type="submit"
+                                            className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                                        >
+                                            <svg
+                                                className="w-4 h-4"
+                                                aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 20 20"
+                                            >
+                                                <path
+                                                    stroke="currentColor"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth={2}
+                                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                                                />
+                                            </svg>
+                                            <span className="sr-only">Търсене</span>
+                                        </button>
+                                    </div>
                                 </div>
                             </form>
 
-                            <div className="flex items-center w-full sm:justify-end mr-2">
+
+                            <div className="flex items-center sm:justify-end mr-2">
                                 <div className="flex pl-2 space-x-1">
-                                    <TrashIcon onClick={() => console.log('click trash') } className="inline-flex items-center w-11 h-11 px-3 py-2 text-sm font-medium text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-2 focus:ring-red-300" />
+                                    <TrashIcon onClick={() => console.log('click trash')} className="inline-flex items-center w-11 h-11 px-3 py-2 text-sm font-medium text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-2 focus:ring-red-300" />
                                 </div>
                             </div>
-                        </div>
 
-                        <button
-                            className="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none"
-                            type="button"
-                            onClick={openDrawerCreate}
-                        >
-                            Добави категория
-                        </button>
+
+                            <button
+                                className="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none"
+                                type="button"
+                                onClick={openDrawerCreate}
+                            >
+                                Добави категория
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
