98589bed183876e28ed13ffdf1a32c136ec87189
Daniel Krastev <danielkrastev01@gmail.com>
2025-05-04 20:04:50 +0300

    Search Inquiry

--- src/components/admin/orders/Orders.jsx
+++ src/components/admin/orders/Orders.jsx
@@ -163,7 +163,6 @@ export default function Orders() {
                                             name="search"
                                             className="block p-3 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                                             placeholder="Име, Email, Телефон..."
-                                            required
                                         />
                                         <button
                                             type="submit"
