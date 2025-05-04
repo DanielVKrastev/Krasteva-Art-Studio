3e791ae094ab79c6c26ca3ab27eaaf5c6697bc00
Daniel Krastev <danielkrastev01@gmail.com>
2025-05-04 20:09:15 +0300

    Search contact message

--- src/components/admin/contact-messages/ContactMessages.jsx
+++ src/components/admin/contact-messages/ContactMessages.jsx
@@ -12,6 +12,8 @@ export default function ContactMessages() {
     const [messages, setMessages] = useState([]);
     const [recordsPerPage, setRecordsPerPage] = useState(10);
     const [isLoading, setIsLoading] = useState(false);
+    const [searchParams, setSearchParams] = useState([]);
+    const [selectedValueCriteria, setSelectedValueCriteria] = useState('telephone');
     
     const [currentPage, setCurrentPage] = useState(1);
 
@@ -39,15 +41,40 @@ export default function ContactMessages() {
     const [isOpenDelete, setIsOpenDelete] = useState(false);
     const [deleteItem, setDeleteItem] = useState(null);
 
-    useEffect(() => {
-        const fetchInitial = async () => {
-            setIsLoading(true);
-            const data = await contactMessageApi.getAll();
-            setIsLoading(false);
-            setMessages(data);
-        };
-        fetchInitial();
-    }, [recordsPerPage, isOpenDelete, isOpenUpdate]);
+useEffect(() => {
+            const fetchInitial = async () => {
+                setIsLoading(true);
+                if (!searchParams.search) {
+                    await contactMessageApi.getAll()
+                        .then(result => {
+                            setMessages(result);
+                            setIsLoading(false);
+                        }).catch(err => {
+                            setIsLoading(false);
+                            console.error(err.message);
+                        });
+                    return;
+                }
+    
+                await contactMessageApi.getAll()
+                    .then(result => {
+                        const search = searchParams.search;
+                        const criteria = searchParams.criteria;
+    
+                        const searchFind = result.filter(order =>
+                            order[criteria]?.toLowerCase().includes(search.toLowerCase())
+                        );
+                        setMessages(searchFind);
+                        setIsLoading(false);
+                    }).catch(err => {
+                        setIsLoading(false);
+                        console.error(err);
+                    });
+                return;
+            };
+    
+            fetchInitial();
+        }, [recordsPerPage, isOpenDelete, isOpenUpdate, searchParams]);
 
     const deleteMessage = async (id) => {
         try{
@@ -78,6 +105,19 @@ export default function ContactMessages() {
         setDeleteItem(null);
     };
 
+    const handleChangeSelectCriteria = (e) => {
+        setSelectedValueCriteria(e.target.value);
+    };
+
+    const searchClickHandler = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const search = formData.get('search');
+        const criteria = formData.get('criteria');
+
+        setSearchParams({ search, criteria });
+    };
+
     return (
         <>
             {isLoading && <LoadingSpinner />}
@@ -95,25 +135,58 @@ export default function ContactMessages() {
 
                     <div className="items-center justify-between block sm:flex">
                         <div className="flex items-center mb-4 sm:mb-0">
-                            <form className="sm:pr-3" action="#" method="GET">
-                                <label htmlFor="products-search" className="sr-only">Търси</label>
-                                <div className="relative w-48 mt-1 sm:w-64 xl:w-96">
-                                    <input
-                                        type="text"
-                                        id="products-search"
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
-                                        placeholder="Търсене на съобщения"
-                                    />
+                            <form className="w-100 mx-auto" onSubmit={searchClickHandler}>
+                                <div className="flex">
+                                    <select
+                                        className="shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 :bg-gray-700"
+                                        name="criteria"
+                                        value={selectedValueCriteria}
+                                        onChange={handleChangeSelectCriteria}
+                                    >
+                                        <option value='firstName'>Име</option>
+                                        <option value='lastName'>Фамилия</option>
+                                        <option value='email'>Email</option>
+                                        <option value='telephone'>Телефон</option>
+                                    </select>
+                                    <div className="relative w-full">
+                                        <input
+                                            type="search"
+                                            name="search"
+                                            className="block p-3 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                                            placeholder="Име, Email, Телефон..."
+                                        />
+                                        <button
+                                            type="submit"
+                                            className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                                        >
+                                            <svg
+                                                className="w-4 h-4"
+                                                aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 20 20"
+                                            >
+                                                <path
+                                                    stroke="currentColor"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth={2}
+                                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                                                />
+                                            </svg>
+                                            <span className="sr-only">Търсене</span>
+                                        </button>
+                                    </div>
                                 </div>
                             </form>
 
-                            <div className="flex items-center w-full sm:justify-end mr-2">
+
+                            <div className="flex items-center sm:justify-end mr-2">
                                 <div className="flex pl-2 space-x-1">
-                                    <TrashIcon onClick={() => console.log('click trash') } className="inline-flex items-center w-11 h-11 px-3 py-2 text-sm font-medium text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-2 focus:ring-red-300" />
+                                    <TrashIcon onClick={() => console.log('click trash')} className="inline-flex items-center w-11 h-11 px-3 py-2 text-sm font-medium text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-2 focus:ring-red-300" />
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
